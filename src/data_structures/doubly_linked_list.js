d96c6511d2a6103b322ec7c3d843defcba7b618f
Kimberley Mackenzie <kimberley@textio.com>
2020-05-21 19:26:23 -0700

    Implement doubly linked list and doubly linked list queue.

--- src/data_structures/doubly_linked_list.js
+++ src/data_structures/doubly_linked_list.js
@@ -31,24 +31,63 @@ class DoublyLinkedList {
   }
 
   insertHead(element) {
+    const newHead = new this.Node({ element });
+
+    this._sentinel.next.prev = newHead;
+    newHead.next = this._sentinel.next;
+    this._sentinel.next = newHead;
+    newHead.prev = this._sentinel;
+    return newHead;
   }
 
   insertTail(element) {
+      const newTail = new this.Node({ element });
+
+      this._sentinel.prev.next = newTail;
+      newTail.prev = this._sentinel.prev;
+      newTail.next = this._sentinel;
+      this._sentinel.prev = newTail;
+      return newTail;
   }
 
   removeHead() {
+    const headToRemove = this._sentinel.next;
+    return this.remove(headToRemove);
   }
 
   removeTail() {
+    const tailToRemove = this._sentinel.prev;
+    return this.remove(tailToRemove);
   }
 
   remove(node) {
+    let nodeElement;
+    if (node && node._active){
+      nodeElement = node.element;
+      node.remove();
+    }
+    return nodeElement;
   }
 
-  forEach(callback) {
+  forEach(callback, container = this) {
+    let node = this._sentinel.next;
+
+    for (let i = 0; node._active; i++) {
+      callback(node.element, i, container);
+      node = node.next;
+    }
   }
 
   count() {
+    let node = this._sentinel.next;
+    let count = 0;
+
+    while (node._active){
+      count += 1;
+      node = node.next;
+    }
+
+    return count;
   }
 }
 
